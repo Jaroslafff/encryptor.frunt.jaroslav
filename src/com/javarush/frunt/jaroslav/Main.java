7a65c0b03bb678c170fae29122ccccd1e3b8ccc2
Jaroslav Frunt <jaroslavfrunt@gmail.com>
2025-01-27 22:30:49 +0200

    Версія на пул реквест

--- src/com/javarush/frunt/jaroslav/Main.java
+++ src/com/javarush/frunt/jaroslav/Main.java
@@ -7,10 +7,6 @@ import com.javarush.frunt.jaroslav.file_engine.FileEngine;
 
 public class Main {
 
-    static String originText;
-    static String processedText;
-    static String decryptedString;
-
     static Cypher cypher = new Cypher();
     static FileEngine fileEngine = new FileEngine();
 
@@ -18,8 +14,8 @@ public class Main {
 
         CommandLine commandLine = new CommandLine(args);
 
-        originText = fileEngine.read(commandLine.getPath());
-        processedText = switch (commandLine.getCypherMode()) {
+        String originText = fileEngine.read(commandLine.getPath());
+        String processedText = switch (commandLine.getCypherMode()) {
             case ENCRYPT -> cypher.encrypt(originText, commandLine.getKey());
             case DECRYPT -> cypher.decrypt(originText, commandLine.getKey());
             case BRUTE_FORCE -> cypher.bruteForce(originText);
